1dd9ce63420babb9962c28ad181977617a4bc4b7
IwanGoodfellow <spaceiwangnibro@gmail.com>
2025-02-27 08:33:54 +0700

    adding some materi-1 postgre

---
+++ materi-1/pipeline.py
@@ -0,0 +1,12 @@
+import sys
+import pandas
+
+# mencetek argumen
+print(sys.argv)
+
+# argumen 0 adl nama file
+# argumen 1 adl argumen yg kita perlukan
+day = sys.argv[1]
+
+# tampilkan kalimat dengan argumen
+print(f'job selesai dengan sukses untuk {day} hari') 
